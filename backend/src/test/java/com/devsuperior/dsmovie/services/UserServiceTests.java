051e6f7eb166c021cd20d7747588ca8e772903f1
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-25 08:57:16 -0300

    UserServiceTests - loadByUsername

--- backend/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ backend/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dsmovie.services;
 
 import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
 import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserDetailsFactory;
 import com.devsuperior.dsmovie.tests.UserFactory;
 import com.devsuperior.dsmovie.utils.CustomUserUtil;
 import org.junit.jupiter.api.Assertions;
@@ -11,10 +13,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
@@ -32,6 +37,7 @@ public class UserServiceTests {
 
     private String existingUsername, nonExistingUsername;
     private UserEntity user;
+    private List<UserDetailsProjection> userDetails;
 
     @BeforeEach
     void setUp() throws Exception{
@@ -39,9 +45,13 @@ public class UserServiceTests {
         nonExistingUsername = "user@gmail.com";
 
         user = UserFactory.createUserEntity();
+        userDetails = UserDetailsFactory.createCustomAdminUser(existingUsername);
 
         Mockito.when(repository.findByUsername(existingUsername)).thenReturn(Optional.of(user));
         Mockito.when(repository.findByUsername(nonExistingUsername)).thenReturn(Optional.empty());
+
+        Mockito.when(repository.searchUserAndRolesByUsername(existingUsername)).thenReturn(userDetails);
+        Mockito.when(repository.searchUserAndRolesByUsername(nonExistingUsername)).thenReturn(new ArrayList<>());
     }
 
 	@Test
@@ -64,9 +74,17 @@ public class UserServiceTests {
 
 	@Test
 	public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
+        UserDetails result = service.loadUserByUsername(existingUsername);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getUsername(), existingUsername);
+
 	}
 
 	@Test
 	public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            service.loadUserByUsername(nonExistingUsername);
+        });
 	}
 }
