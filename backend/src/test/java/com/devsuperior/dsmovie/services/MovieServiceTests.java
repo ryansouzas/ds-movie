2d7e8cd1ad2636d37d71f868f97c7bc7dab78796
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-25 19:42:54 -0300

    MovieServiceTests - findAllTest

--- backend/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ backend/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,18 +1,54 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
+
+    @Mock
+    private MovieRepository repository;
+
+    private PageImpl<MovieEntity> page;
+    private Pageable pageable;
+    private MovieEntity movie;
+    private String existingTitle;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingTitle = "Test Movie";
+        movie = MovieFactory.createMovieEntity();
+        pageable = PageRequest.of(0,10);
+        page = new PageImpl<>(Collections.singletonList(movie));
+
+        Mockito.when(repository.searchByTitle(existingTitle, pageable)).thenReturn(page);
+    }
 	
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+        Page<MovieDTO> result = service.findAll(existingTitle, pageable);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getContent().getFirst().getTitle(), existingTitle);
 	}
 	
 	@Test
--- backend/src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ backend/src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,15 +1,58 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
 	
 	@InjectMocks
 	private ScoreService service;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private MovieRepository movieRepository;
+
+    @Mock
+    private ScoreRepository scoreRepository;
+
+    private UserEntity user;
+    private MovieEntity movie;
+    private ScoreDTO scoreDTO;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        user = UserFactory.createUserEntity();
+        scoreDTO = ScoreFactory.createScoreDTO();
+        movie = MovieFactory.createMovieEntity();
+
+        Mockito.when(userService.authenticated()).thenReturn(user);
+        Mockito.when(movieRepository.findById(scoreDTO.getMovieId())).thenReturn(Optional.of(movie));
+        Mockito.when(movieRepository.findById(scoreDTO.getMovieId())).thenReturn(Optional.empty());
+        Mockito.when(movieRepository.save(any())).thenReturn(movie);
+    }
 	
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
