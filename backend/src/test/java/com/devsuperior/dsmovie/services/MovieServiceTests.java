64b2ce40a6fe46b18b1e88cd657e51bc5b8f0c9b
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-26 05:45:51 -0300

    MovieServiceTests - findByIdTests

--- backend/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ backend/src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmovie.services;
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.entities.MovieEntity;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dsmovie.tests.MovieFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
@@ -28,19 +30,25 @@ public class MovieServiceTests {
     @Mock
     private MovieRepository repository;
 
+
     private PageImpl<MovieEntity> page;
     private Pageable pageable;
     private MovieEntity movie;
     private String existingTitle;
+    private Long existingId, nonExistingId;
 
     @BeforeEach
     void setUp() throws Exception{
         existingTitle = "Test Movie";
+        existingId = 1L;
+        nonExistingId = 2L;
         movie = MovieFactory.createMovieEntity();
         pageable = PageRequest.of(0,10);
         page = new PageImpl<>(Collections.singletonList(movie));
 
         Mockito.when(repository.searchByTitle(existingTitle, pageable)).thenReturn(page);
+        Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(movie));
+        Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
     }
 	
 	@Test
@@ -53,10 +61,17 @@ public class MovieServiceTests {
 	
 	@Test
 	public void findByIdShouldReturnMovieDTOWhenIdExists() {
+        MovieDTO result = service.findById(existingId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(existingId, result.getId());
 	}
 	
 	@Test
 	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.findById(nonExistingId);
+        });
 	}
 	
 	@Test
